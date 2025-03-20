f5fdc7951c38264a7956c6a33a1e1c6a0fda4905
h2jie <hangjie.huang@estudiantat.upc.edu>
2025-03-20 23:43:59 +0100

    Combat service fixed

--- src/modules/combats/combat_service.ts
+++ src/modules/combats/combat_service.ts
@@ -1,12 +1,32 @@
-// src/services/user_service.ts
+
+import mongoose from 'mongoose';
 import Combat, { ICombat } from '../combats/combat_models.js';
 
 export const saveMethod = () => {
     return 'Hola';
 };
 export const createCombat = async (combatData: ICombat) => {
-    const combat = new Combat(combatData);
-    return await combat.save();
+    try {
+        // Asegúrate de que gym es un ObjectId válido.
+        if (typeof combatData.gym === 'string') {
+            combatData.gym = new mongoose.Types.ObjectId(combatData.gym);
+        }
+        
+        // Asegúrate de que boxers es un array ObjectId válido.
+        if (Array.isArray(combatData.boxers)) {
+            combatData.boxers = combatData.boxers.map(id => 
+                typeof id === 'string' ? new mongoose.Types.ObjectId(id) : id
+            );
+        }
+        
+        console.log('Datos de combate procesados:', combatData);
+        
+        const combat = new Combat(combatData);
+        return await combat.save();
+    } catch (error) {
+        console.error('Createcombat error:', error);
+        throw error;
+    }
 };
 
 export const getAllCombats = async (page: number, pageSize: number) => {
